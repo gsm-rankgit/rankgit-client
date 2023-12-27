d61523825647540c239b3ab656f338d5af140ee9
kimgh06 <kkh061101@naver.com>
2023-12-27 18:26:39 +0900

    usercontainer width set

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,21 +12,23 @@ export default function Home() {
       <span>Issue</span>
       <span>Review</span>
     </div>
-    <div className="users">
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={1} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={2} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={3} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={4} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={5} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={6} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={7} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={8} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={9} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={10} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={11} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={12} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={13} />
-      <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={14} />
+    <div className="userContainer">
+      <div className="users">
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={1} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={2} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={3} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={4} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={5} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={6} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={7} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={8} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={9} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={10} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={11} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={12} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={13} />
+        <UserInfo img={''} name={'esperar'} github={'https://github.com/esperar'} commit={9999} pr={99} issue={99} review={99} ranking={14} />
+      </div>
     </div>
     <Footer />
   </div>
